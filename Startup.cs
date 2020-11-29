f7b77994016048ecbe0cd6a3071d1263c6ce219e
ggraur <39068501+ggraur@users.noreply.github.com>
2020-11-29 22:56:11 +0000

    adding migration

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using toDoList.Models;
-using toDoManagementDataAccess;
+
 
 namespace toDoList
 {
